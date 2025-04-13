4f0699ab2425d7c1769100ef6002763b473ca672
Thu Bui <buithithundh2003@gmail.com>
2025-04-13 08:40:27 +0700

    deploy2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require("./index");
 const { connectDb } = require("./config/db");
-const PORT = 4000;
+// const dotenv = require('dotenv');
+require('dotenv').config();
+const PORT = process.env.PORT||4000;
 app.listen(PORT, ()=>{
     console.log("Running on port "+PORT);
 })
